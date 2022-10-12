557fb5c8d7aa3e67eba4922cd9af5c2d430c4fd3
ahmedgamalpro <ahmedgamal.pro1@gmail.com>
2022-10-12 16:33:36 +0200

    add files

---
+++ src/components/AllInOne.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import { CheckIcon } from "@heroicons/react/outline";
+
+const AllInOne = () => {
+    return (
+        <div className="w-full my-32">
+            <div className="max-w-[1240px] mx-auto px-2">
+                <h2 className="text-5xl font-bold text-center">All-In-One Platform</h2>
+                <p className="text-2xl py-8 text-gray-500 text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, eum. Harum facilis reprehenderit quia molestias neque! Minima expedita nemo, voluptas eaque nobis fugit, rerum eum atque quo ad non nisi.</p>
+            
+                <div className="grid sm:grid-cols-2 lg:grid-cols-4 gab-4 pt-4">
+                    <div className="flex">
+                        <div>
+                            <CheckIcon className="w-7 mr-4 text-green-600" />
+                        </div>
+                        
+                        <div>
+                            <h3 className="font-bold text-lg">Noteifications</h3>
+                            <p className="text-lg pt-2 pb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam labore aliquam obcaecati veritatis harum alias fugiat maiores nam atque animi commodi esse cumque repellendus voluptatem quidem, distinctio enim non corrupti!</p>
+                        </div>
+                        
+                    </div>
+                    <div className="flex">
+                        <div>
+                            <CheckIcon className="w-7 mr-4 text-green-600" />
+                        </div>
+                        
+                        <div>
+                            <h3 className="font-bold text-lg">Noteifications</h3>
+                            <p className="text-lg pt-2 pb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam labore aliquam obcaecati veritatis harum alias fugiat maiores nam atque animi commodi esse cumque repellendus voluptatem quidem, distinctio enim non corrupti!</p>
+                        </div>
+                        
+                    </div>
+                    <div className="flex">
+                        <div>
+                            <CheckIcon className="w-7 mr-4 text-green-600" />
+                        </div>
+                        
+                        <div>
+                            <h3 className="font-bold text-lg">Noteifications</h3>
+                            <p className="text-lg pt-2 pb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam labore aliquam obcaecati veritatis harum alias fugiat maiores nam atque animi commodi esse cumque repellendus voluptatem quidem, distinctio enim non corrupti!</p>
+                        </div>
+                        
+                    </div>
+                    <div className="flex">
+                        <div>
+                            <CheckIcon className="w-7 mr-4 text-green-600" />
+                        </div>
+                        
+                        <div>
+                            <h3 className="font-bold text-lg">Noteifications</h3>
+                            <p className="text-lg pt-2 pb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam labore aliquam obcaecati veritatis harum alias fugiat maiores nam atque animi commodi esse cumque repellendus voluptatem quidem, distinctio enim non corrupti!</p>
+                        </div>
+                        
+                    </div>
+                    <div className="flex">
+                        <div>
+                            <CheckIcon className="w-7 mr-4 text-green-600" />
+                        </div>
+                        
+                        <div>
+                            <h3 className="font-bold text-lg">Noteifications</h3>
+                            <p className="text-lg pt-2 pb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam labore aliquam obcaecati veritatis harum alias fugiat maiores nam atque animi commodi esse cumque repellendus voluptatem quidem, distinctio enim non corrupti!</p>
+                        </div>
+                        
+                    </div>
+                    <div className="flex">
+                        <div>
+                            <CheckIcon className="w-7 mr-4 text-green-600" />
+                        </div>
+                        
+                        <div>
+                            <h3 className="font-bold text-lg">Noteifications</h3>
+                            <p className="text-lg pt-2 pb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam labore aliquam obcaecati veritatis harum alias fugiat maiores nam atque animi commodi esse cumque repellendus voluptatem quidem, distinctio enim non corrupti!</p>
+                        </div>
+                        
+                    </div>
+                    <div className="flex">
+                        <div>
+                            <CheckIcon className="w-7 mr-4 text-green-600" />
+                        </div>
+                        
+                        <div>
+                            <h3 className="font-bold text-lg">Noteifications</h3>
+                            <p className="text-lg pt-2 pb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam labore aliquam obcaecati veritatis harum alias fugiat maiores nam atque animi commodi esse cumque repellendus voluptatem quidem, distinctio enim non corrupti!</p>
+                        </div>
+                        
+                    </div>
+                    <div className="flex">
+                        <div>
+                            <CheckIcon className="w-7 mr-4 text-green-600" />
+                        </div>
+                        
+                        <div>
+                            <h3 className="font-bold text-lg">Noteifications</h3>
+                            <p className="text-lg pt-2 pb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam labore aliquam obcaecati veritatis harum alias fugiat maiores nam atque animi commodi esse cumque repellendus voluptatem quidem, distinctio enim non corrupti!</p>
+                        </div>
+                        
+                    </div>
+                    <div className="flex">
+                        <div>
+                            <CheckIcon className="w-7 mr-4 text-green-600" />
+                        </div>
+                        
+                        <div>
+                            <h3 className="font-bold text-lg">Noteifications</h3>
+                            <p className="text-lg pt-2 pb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam labore aliquam obcaecati veritatis harum alias fugiat maiores nam atque animi commodi esse cumque repellendus voluptatem quidem, distinctio enim non corrupti!</p>
+                        </div>
+                        
+                    </div>
+
+
+
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AllInOne
